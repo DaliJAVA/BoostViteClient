8481da955f033576e5a96f2c561b609cee8494c1
DaliJAVA <40804762+DaliJAVA@users.noreply.github.com>
2020-11-23 12:15:03 +0100

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  totalAngularPackages: number;
+  easyDem: number;
+
+  constructor(private http: HTTP) { }
+
+  getUrl() {
+    console.log("Start");
+    var url ='https://jsonplaceholder.typicode.com/todos/1'
+      this.http.get(url, {}, {}).then(response=> {
+        console.log(response.status);
+        console.log(JSON.parse(response.data)); // JSON data returned by server
+        console.log(response.headers);
+  });
+
+}
+}
